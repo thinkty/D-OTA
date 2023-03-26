4737c99ebd34a15da335d4aecbd3d9020fab27be
thinkty <the.thinkty@gmail.com>
2023-03-25 20:22:53 -0400

    Dividing tasks (DHT & CDS) into two separate files

--- main/app_main.c
+++ main/app_main.c
@@ -4,9 +4,8 @@
 #include "lwip/err.h"
 #include "lwip/sys.h"
 
-#include "app_ap.h"
-#include "app_http.h"
-#include "app_tasks.h"
+#include "app_task_dht.h"
+#include "app_task_cds.h"
 #include "app_ota.h"
 
 esp_ota_firm_t ota_firm;
@@ -15,34 +14,17 @@ void app_main()
 {
     ESP_ERROR_CHECK(nvs_flash_init());
     
-    // Create event loop to handle WiFi and HTTP events
+    // Create event loop to handle TCP events in various tasks
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-    // Initialize the access point
-    init_ap();
-
     // Initialize OTA-update
-    ESP_ERROR_CHECK(init_ota(&ota_firm));
-
-    // Initialize HTTP server for the webpage to upload updates
-    init_http();
-
-    // Interval (in milliseconds) to read/print sensor values.
-    task_args th_args = {
-        .interval = 10000,
-        .pin = CONFIG_APP_AM2302_GPIO
-    };
+    // ESP_ERROR_CHECK(init_ota());
 
     // Initialize and run the temperature/humidity sensor task
-    ESP_ERROR_CHECK(init_th(th_args.pin));
-    xTaskCreate(th_task, "th task", DEFAULT_THREAD_STACKSIZE, &th_args, DEFAULT_THREAD_PRIO, NULL);
-
-    task_args ll_args = {
-        .interval = 10000,
-        .pin = CONFIG_APP_CDS_GPIO
-    };
+    ESP_ERROR_CHECK(init_dht());
+    xTaskCreate(task_dht, "task_dht", DEFAULT_THREAD_STACKSIZE, NULL, DEFAULT_THREAD_PRIO, NULL);
 
     // Initialize and run the light sensor task
-    ESP_ERROR_CHECK(init_ll(ll_args.pin));
-    xTaskCreate(ll_task, "ll task", DEFAULT_THREAD_STACKSIZE, &ll_args, DEFAULT_THREAD_PRIO, NULL);
+    ESP_ERROR_CHECK(init_cds());
+    xTaskCreate(task_cds, "task_cds", DEFAULT_THREAD_STACKSIZE, NULL, DEFAULT_THREAD_PRIO, NULL);
 }
